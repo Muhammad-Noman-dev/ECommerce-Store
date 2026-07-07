13e975caa50fa9ca705dbf0acf6b273e9eb3b9a5
Muhammad Noman <nomi.qasim7788@gmail.com>
2026-07-08 01:51:19 +0500

    backend update

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,18 +1,20 @@
 const multer = require("multer");
-
+const path = require("path");
 
 const storage = multer.diskStorage({
+
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, path.join(__dirname, "../uploads"));
   },
 
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
   },
+
 });
 
 const upload = multer({
-  storage: storage,
+  storage,
 });
 
 module.exports = upload;
